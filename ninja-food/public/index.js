4b8486326fd80339a8051ae0b6f57fecd7298999
Marina Oliveira <marina09oliveira@gmail.com>
2021-01-04 16:45:16 -0300

    ninja-food

---
+++ ninja-food/public/index.js
@@ -0,0 +1,10 @@
+const burguer = document.querySelector('#burguer');
+const menu = document.querySelector('#menu');
+
+burguer.addEventListener('click', () => {
+  if (menu.classList.contains('hidden')) {
+    menu.classList.remove('hidden');
+  } else {
+    menu.classList.add('hidden');
+  }
+});
